493e7b245a2e657902ddd25b5f613d431d1a0b22
Andre Costa <andrechougames1337kpopcosta@gmail.com>
2024-06-05 21:53:24 +0100

    update all

---
+++ Modpack 1.4.7/.minecraft/config/CustomStuff/mods/choukpop/mod.js
@@ -0,0 +1,27 @@
+
+// Energy Crystal
+mod.addRecipe("30241:27 1", 3, 3, 331, 331, 331, 331, ":gemRuby", 331, 331, 331, 331);
+mod.addRecipe("30241:27 1", 3, 3, 331, 331, 331, 331, 31933, 331, 331, 331, 331);
+
+// Lapotron Crystal
+mod.addRecipe("30240:27 1", 3, 3, 22, 30191, 22, 30241, 264, 30241, 22, 30191, 22);
+mod.addRecipe("30240:27 1", 3, 3, 22, 30191, 22, 30241, 31927, 30241, 22, 30191, 22);
+mod.addRecipe("30240:27 1", 3, 3, 22, 30191, 22, 30241, ":gemSapphire", 30241, 22, 30191, 22);
+
+// Convert Ruby
+mod.addRecipe("31933 1", 1, 1, ":gemRuby");
+mod.addRecipe("9258 1", 1, 1, 31933);
+
+// Convert Sapphire
+mod.addRecipe("31932 1", 1, 1, ":gemSapphire");
+mod.addRecipe("9258:2 1", 1, 1, 31932);
+
+// Convert Green Sapphire
+mod.addRecipe("31927 1", 1, 1, ":gemGreenSapphire");
+mod.addRecipe("9258:1 1", 1, 1, 31927);
+
+// Valve Pipe
+mod.addRecipe("10456 1", 3, 1, 19436, 69, 19436);
+
+// Sticky Resin
+mod.addRecipe("30217 1", 3, 3, 12258, 12258, 12258, 12258, 12258, 12258, 12258, 12258, 12258);
